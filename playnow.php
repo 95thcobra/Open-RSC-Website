9fe123557f33e2781c802658aae8a360e599660f
Marwolf <cleako@gmail.com>
2018-07-08 16:21:52 -0400

    Added OpenJDK instruction

--- playnow.php
+++ playnow.php
@@ -9,6 +9,8 @@
                         <h4><b><a href="https://discordapp.com/invite/94vVKND"/>-> Join the Discord chat 🔪</a><br /><br />
                         <a href="/board/ucp.php?mode=register"/>--> Register a forum account 🏹</a><br /><br />
                         <a href="/downloads/Open_RSC_Launcher.jar"/>---> Download the game launcher 🐺</a></b></h4>
+                    
+                    <br/><br/><p>If you are using OpenJDK on Linux, run this: "<b>sudo apt install openjfx</b>"</p>
                 </article>
         </div>
 </div>
