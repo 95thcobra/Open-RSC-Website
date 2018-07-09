bfffe95a5919d554c4f6dc0c037cde4a89a52bac
Marwolf <cleako@gmail.com>
2018-07-09 12:54:25 -0400

    Added link to download Java

--- playnow.php
+++ playnow.php
@@ -9,7 +9,8 @@
                         <h4><b><a href="https://discordapp.com/invite/94vVKND"/>-> Join the Discord chat 🔪</a><br /><br />
                         <a href="/board/ucp.php?mode=register"/>--> Register a forum account 🏹</a><br /><br />
                         <a href="/downloads/Launcher.jar"/>---> Download the game launcher 🐺</a></b></h4>
-                    <br/><br/><p>If you are using OpenJDK on Linux, run this command first: "<b>sudo apt install openjfx</b>"</p>
+                    <br /><br /><p>Not able to open the Launcher? <a href="http://www.oracle.com/technetwork/java/javase/downloads/jre10-downloads-4417026.html"/>Get the latest Java runtime environment here.</a>
+                        <br /><br />If you are using OpenJDK on Linux, run this command first: "<b>sudo apt install openjfx</b>"</p>
                 </article>
         </div>
 </div>
